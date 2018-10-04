e04e5232f23d926464adcf7fd59513f19f671200
christophercameronco <30921720+christophercameronco@users.noreply.github.com>
2018-10-04 15:46:21 -0400

    python

---
+++ person.py
@@ -0,0 +1,25 @@
+empty_line=("\n")
+person ={
+    'first_name': 'Christopher',
+    'last_name': 'Cameron',
+    'age': 27,
+    'city': 'Erwin'}
+    
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
+print(empty_line)
+
+favorite_numbers={
+    'Christopher': 90,
+    'Peggy': 54,
+    'Andre': 94,
+    'Jerry':57}
+    
+print(favorite_numbers)
+print(empty_line)
+
+
+
+
