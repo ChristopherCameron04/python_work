24f21fb8b09eedb529ff51b10877263a6349f7f1
christophercameronco <30921720+christophercameronco@users.noreply.github.com>
2018-10-07 16:32:27 -0400

    python

---
+++ glossary.py
@@ -0,0 +1,15 @@
+python_words={
+'print': 'print to console',
+'while': 'controlling the flow of the program',
+'if': 'used to determine, which statements are going to be executed',
+'not': 'negates a boolean value',
+'and': 'all condtions in a boolean expression must be met',
+'or': 'at least one condition must be met',
+'def': 'used to create anew user defined function',
+'lambda': 'creates a new anonymous function',
+'class': 'used tfor debuggin purposes',
+'yield': 'is used with generators'}
+
+for word, definition in python_words.items():
+    print("\nWord: " + word)
+    print("Definition: " + definition)  
