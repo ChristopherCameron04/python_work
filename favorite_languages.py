de27e8426920447943b409e69124117fd003681a
christophercameronco <30921720+christophercameronco@users.noreply.github.com>
2018-10-07 15:18:08 -0400

    python

--- favorite_languages.py
+++ favorite_languages.py
@@ -1,8 +1,41 @@
+empty_line = ("\n")
 favorite_languages= {
     'jen': 'python',
     'sarah': 'c',
     'edward': 'ruby',
     'phil':'python',
     }
-print("Sarah's favorite language is "+ 
-    favorite_languages['sarah'].title()+ ".")
+for name, language in favorite_languages.items():
+    print(name.title() +"'s favorite language is " + language.title()
+    + "." + "\n")
+    
+for name in favorite_languages.keys():
+    print(name.title())
+    
+print(empty_line)
+
+friends = ['jen', 'edward']
+for name in favorite_languages.keys():
+    print(name.title())
+    
+    if name in friends:
+        print("Hi! " + name.title() + 
+        ", I see your favorite language is " + 
+        favorite_languages[name].title() +"!") 
+if 'erin' not in favorite_languages.keys():
+        print("\nErin, please take our poll!\n")
+    
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.\n")
+    
+print("The following languages have been mentioned: ")
+for language in favorite_languages.values():
+    print(language.title())
+    
+print("\nThe following languages have been mentioned: ")
+for language in set(favorite_languages.values()):
+    print(language.title())
+    
+    
+
+
